056ad806c6ccdf872fb75bbea7fc29099b5361b8
Dubem Ezeagwu <48288857+dubemezeagwu@users.noreply.github.com>
2024-04-16 20:02:39 +0100

    feat: preventing parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 // const bodyParser = require("body-parser");
 
 const AppError = require("./utils/appError");
@@ -41,6 +42,19 @@ app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
 
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
